318e01da85716c8d94193743be08b3e89edf7508
lucasoliveira04 <camposdlucasoli@gmail.com>
2024-05-18 11:02:34 -0300

    twoSum

--- twoSum/index.c
+++ twoSum/index.c
@@ -8,18 +8,16 @@ int main(){
 
     size_t length = sizeof(nums)/sizeof(nums[0]);
 
-    size_t i, j, k;
+    size_t i, j;
 
     printf("Valor alvo: ");
     scanf("%d", &target);
 
     for (i = 0; i < length; i++){
         for (j = 0; j < length; j++){
-            for (k = 0; k < length; k++){
-                if (nums[i] + nums[j] + nums[k] == target){
-                    printf("%d + %d + %d = %d\n", nums[i], nums[j], nums[k], target);
-                    return 0;
-                }    
+            if (nums[i] + nums[j] == target){
+                printf("%d + %d = %d\n", nums[i], nums[j], target);
+                return 0;
             }
         }
     }
